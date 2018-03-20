d3346e9cb67f7995fc97a3976e2052af0f521d3c
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-03-20 00:22:17 -0300

    Add utils methods for image operation
    - Implement normalization function

--- src/main/java/ar/edu/itba/ati/ati_soft/service/ImageOperationServiceImpl.java
+++ src/main/java/ar/edu/itba/ati/ati_soft/service/ImageOperationServiceImpl.java
@@ -2,7 +2,9 @@ package ar.edu.itba.ati.ati_soft.service;
 
 import ar.edu.itba.ati.ati_soft.interfaces.ImageOperationService;
 import ar.edu.itba.ati.ati_soft.models.Image;
+import ar.edu.itba.ati.ati_soft.utils.QuadConsumer;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
@@ -12,6 +14,9 @@ import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -23,34 +28,147 @@ public class ImageOperationServiceImpl implements ImageOperationService {
     @Override
     public Image getNegative(Image image) {
         final BufferedImage content = image.getContent();
-        final WritableRaster newRaster = getNegativeRaster(content);
-        final ColorModel colorModel = content.getColorModel();
-        final boolean preMultiplied = content.isAlphaPremultiplied();
-        final Hashtable<String, Object> properties = getProperties(content);
-        final BufferedImage newContent = new BufferedImage(colorModel, newRaster, preMultiplied, properties);
+        final WritableRaster newRaster = getNegativeRaster(content.getRaster());
+        final BufferedImage newContent = generateNewBufferedImage(newRaster, content);
         return new Image(newContent);
     }
 
     /**
-     * Calculates the negative raster for the given {@link BufferedImage}.
+     * Creates a {@link WritableRaster} from the given {@code raster}, applying the normalization function.
      *
-     * @param bufferedImage The {@link BufferedImage} whose negative must be calculated.
-     * @return A {@link WritableRaster} with the negative of the given {@code bufferedImage}.
+     * @param raster The {@link Raster} to be normalized.
+     * @return The new {@link WritableRaster} (i.e the given {@link Raster} with the normalization function applied).
      */
-    private static WritableRaster getNegativeRaster(BufferedImage bufferedImage) {
-        final Raster raster = bufferedImage.getRaster();
-        final WritableRaster newRaster = Raster.createWritableRaster(raster.getSampleModel(), null);
+    private static WritableRaster getNormalized(Raster raster) {
+        final int bands = raster.getNumDataElements();
+        final int maximums[] = IntStream.range(0, bands).map(i -> 0).toArray();
+        final int minimums[] = IntStream.range(0, bands).map(i -> 0).toArray();
+        populateWithMinAndMax(raster, minimums, maximums);
+        final double[] factors = IntStream.range(0, bands).mapToDouble(i -> 1 / (maximums[i] - minimums[i])).toArray();
+        return createApplying(raster,
+                (value, i) -> ((double) value - minimums[i]) * factors[i],
+                Double[]::new,
+                (r, x, y, p) -> r.setPixel(x, y, toPrimitiveArray(p)));
+    }
+
+
+    /**
+     * Populates the given {@code minimums} and {@code maximums} arrays
+     * with the minimums and maximums values for each band.
+     *
+     * @param raster   The {@link Raster} to which the calculation will be performed.
+     * @param minimums An array to which the minimum value for each band will be saved.
+     * @param maximums An array to which the maximum value for each band will be saved.
+     * @throws IllegalArgumentException If any of the arrays are null, or if both arrays don'thave the same length.
+     */
+    private static void populateWithMinAndMax(Raster raster, final int minimums[], final int maximums[])
+            throws IllegalArgumentException {
+        Assert.notNull(minimums, "The minimums array must not be null");
+        Assert.notNull(maximums, "The maximums array must not be null");
+        if (minimums.length != maximums.length) {
+            throw new IllegalArgumentException("Both minimums and maximums array must have the same length");
+        }
+        final int bands = minimums.length; // Using minimums, but is the same to use maximums.
         for (int x = 0; x < raster.getWidth(); x++) {
             for (int y = 0; y < raster.getHeight(); y++) {
-                final int[] pixel = new int[newRaster.getNumDataElements()];
-                for (int i = 0; i < pixel.length; i++) {
+                for (int i = 0; i < bands; i++) {
                     final int value = raster.getSample(x, y, i);
-                    pixel[i] = 0xFF - value;
+                    maximums[i] = maximums[i] > value ? maximums[i] : value;
+                    minimums[i] = minimums[i] < value ? minimums[i] : value;
                 }
-                newRaster.setPixel(x, y, pixel);
+            }
+        }
+    }
+
+    /**
+     * Creates a new {@link WritableRaster} using as base the given {@link Raster},
+     * applying the given {@code changer} {@link BiFunction} to each pixel,
+     * using the given {@code pixelGenerator} to create a pixel,
+     * and using the given {@code setter} {@link QuadConsumer} to set values in a {@link WritableRaster}.
+     *
+     * @param raster         The base {@link Raster}.
+     * @param changer        The {@link BiFunction} to apply to each pixel,
+     *                       being the first element the original pixel value, and the second element, the band.
+     * @param pixelGenerator An {@link IntFunction} that will generate pixels.
+     * @param setter         A {@link QuadConsumer} which defines how a {@link WritableRaster} is set a {@code T} value.
+     * @param <T>            Concrete type of value for each pixel.
+     * @return The new {@link WritableRaster}.
+     */
+    private static <T> WritableRaster createApplying(Raster raster,
+                                                     BiFunction<Integer, Integer, T> changer,
+                                                     IntFunction<T[]> pixelGenerator,
+                                                     QuadConsumer<WritableRaster, Integer, Integer, T[]> setter) {
+        final WritableRaster newRaster = Raster.createWritableRaster(raster.getSampleModel(), null);
+        final int bands = raster.getNumDataElements();
+        for (int x = 0; x < raster.getWidth(); x++) {
+            for (int y = 0; y < raster.getHeight(); y++) {
+                final T[] newPixel = pixelGenerator.apply(bands);
+                for (int i = 0; i < bands; i++) {
+                    final int value = raster.getSample(x, y, i);
+                    newPixel[i] = changer.apply(value, i);
+                }
+                setter.accept(newRaster, x, y, newPixel);
             }
         }
         return newRaster;
+    }
+
+
+    /**
+     * Calculates the negative raster for the given {@link BufferedImage}.
+     *
+     * @param raster The base {@link Raster}.
+     * @return A {@link WritableRaster} with the negative of the given {@code bufferedImage}.
+     */
+    private static WritableRaster getNegativeRaster(Raster raster) {
+        return createApplying(raster,
+                (value, i) -> /*0xFF -*/ 2 * value,
+                Integer[]::new,
+                (r, x, y, p) -> r.setPixel(x, y, toPrimitiveArray(p)));
+    }
+
+    /**
+     * Transforms the given {@code original} array into a primitive array.
+     *
+     * @param original The original array to be copied.
+     * @return The new Array.
+     */
+    private static int[] toPrimitiveArray(Integer[] original) {
+        final int[] result = new int[original.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = original[i];
+        }
+        return result;
+    }
+
+    /**
+     * Transforms the given {@code original} array into a primitive array.
+     *
+     * @param original The original array to be copied.
+     * @return The new Array.
+     */
+    private static double[] toPrimitiveArray(Double[] original) {
+        final double[] result = new double[original.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = original[i];
+        }
+        return result;
+    }
+
+
+    /**
+     * Generates a new {@link BufferedImage}, using the given {@link WritableRaster},
+     * and taking properties from the {@code base} {@link BufferedImage}.
+     *
+     * @param writableRaster The {@link WritableRaster} to be used to generate the new {@link BufferedImage}.
+     * @param base           A {@link BufferedImage} from which properties will be taken.
+     * @return The new {@link BufferedImage}.
+     */
+    private BufferedImage generateNewBufferedImage(WritableRaster writableRaster, BufferedImage base) {
+        final ColorModel colorModel = base.getColorModel();
+        final boolean preMultiplied = base.isAlphaPremultiplied();
+        final Hashtable<String, Object> properties = getProperties(base);
+        return new BufferedImage(colorModel, writableRaster, preMultiplied, properties);
     }
 
     /**
