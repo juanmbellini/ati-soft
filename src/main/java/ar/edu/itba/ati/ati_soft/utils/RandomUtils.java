c254ff88332a57339ae653920960f386675d0b7d
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-04-01 00:01:56 -0300

    Add a Rayleigh generator
    - Also fix bug in gaussian
    - Also add parameter check to gaussian

--- src/main/java/ar/edu/itba/ati/ati_soft/utils/RandomUtils.java
+++ src/main/java/ar/edu/itba/ati/ati_soft/utils/RandomUtils.java
@@ -1,5 +1,7 @@
 package ar.edu.itba.ati.ati_soft.utils;
 
+import org.springframework.util.Assert;
+
 import java.util.Random;
 
 /**
@@ -28,6 +30,34 @@ public class RandomUtils {
      * @return The generated pseudo random number.
      */
     public static double randomGauss(double mean, double standardDeviation, Random random) {
-        return random.nextGaussian() * standardDeviation * mean;
+        Assert.notNull(random, "The Random instance must not be null.");
+        Assert.isTrue(standardDeviation > 0, "The standard deviation must be positive");
+        return random.nextGaussian() * standardDeviation + mean;
+    }
+
+    /**
+     * Generates pseudorandom numbers with Rayleigh distribution.
+     *
+     * @param xi The distribution's xi parameter.
+     * @return The generated pseudo random number.
+     */
+    public static double randomRayleigh(double xi) {
+        return randomRayleigh(xi, new Random());
+    }
+
+    /**
+     * Generates pseudorandom numbers with Gaussian distribution,
+     * using as base generator the given {@link Random} instance.
+     *
+     * @param xi     The distribution's scale parameter (i.e the xi value).
+     * @param random The {@link Random} instance to be used to generate the distribution.
+     * @return The generated pseudo random number.
+     */
+    public static double randomRayleigh(double xi, Random random) {
+        Assert.notNull(random, "The Random instance must not be null.");
+        Assert.isTrue(xi > 0, "The scale parameter must be positive");
+        // Subtract Random#nextDouble to 1, in order to have a value between 0 (exclusive) and 1 (inclusive).
+        final double randomVariate = 1 - random.nextDouble();
+        return xi * Math.sqrt(-2 * Math.log(randomVariate));
     }
 }
