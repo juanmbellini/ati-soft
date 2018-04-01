cba07200b1493c1978a749da4099903a16ffbcc7
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-04-01 03:12:37 -0300

    Add more noise generators
    - Add multiplicative rayeligh noise generator
    - Add multiplicative exponential noise generator

--- src/main/java/ar/edu/itba/ati/ati_soft/interfaces/NoiseGenerationService.java
+++ src/main/java/ar/edu/itba/ati/ati_soft/interfaces/NoiseGenerationService.java
@@ -8,7 +8,7 @@ import ar.edu.itba.ati.ati_soft.models.Image;
 public interface NoiseGenerationService {
 
     /**
-     * Adds Gaussian noise to the given {@link Image}.
+     * Adds Additive Gaussian noise to the given {@link Image}.
      *
      * @param image             The {@link Image} to pollute.
      * @param mean              The mean (i.e mu parameter)
@@ -17,5 +17,25 @@ public interface NoiseGenerationService {
      *                          used to generate pseudorandom numbers with a Gaussian distribution.
      * @return A new {@link Image} instance with Additive Gaussian noise added to it.
      */
-    Image addAdditiveGaussianNoise(Image image, double mean, double standardDeviation);
+    Image additiveGaussianNoise(Image image, double mean, double standardDeviation);
+
+    /**
+     * Adds Multiplicative Rayleigh noise to the given {@link Image}.
+     *
+     * @param image The {@link Image} to pollute.
+     * @param scale The scale parameter (i.e the xi parameter)
+     *              used to generate pseudorandom numbers with a Gaussian distribution.
+     * @return A new {@link Image} instance with  Multiplicative Rayleigh noise added to it.
+     */
+    Image multiplicativeRayleighNoise(Image image, double scale);
+
+    /**
+     * Adds Multiplicative Exponential noise to the given {@link Image}.
+     *
+     * @param image The {@link Image} to pollute.
+     * @param rate  The rate parameter (i.e the lambda parameter)
+     *              used to generate pseudorandom numbers with a Gaussian distribution.
+     * @return A new {@link Image} instance with Multiplicative Exponential noise added to it.
+     */
+    Image multiplicativeExponentialNoise(Image image, double rate);
 }
--- src/main/java/ar/edu/itba/ati/ati_soft/service/NoiseGenerationServiceImpl.java
+++ src/main/java/ar/edu/itba/ati/ati_soft/service/NoiseGenerationServiceImpl.java
@@ -12,8 +12,18 @@ import org.springframework.stereotype.Service;
 public class NoiseGenerationServiceImpl implements NoiseGenerationService {
 
     @Override
-    public Image addAdditiveGaussianNoise(Image image, double mean, double standardDeviation) {
+    public Image additiveGaussianNoise(Image image, double mean, double standardDeviation) {
         return ImageManipulationHelper.createApplying(image,
                 (x, y, b, v) -> v + RandomUtils.randomGauss(mean, standardDeviation));
+    }
+
+    @Override
+    public Image multiplicativeRayleighNoise(Image image, double scale) {
+        return ImageManipulationHelper.createApplying(image, (x, y, b, v) -> v * RandomUtils.randomRayleigh(scale));
+    }
+
+    @Override
+    public Image multiplicativeExponentialNoise(Image image, double rate) {
+        return ImageManipulationHelper.createApplying(image, (x, y, b, v) -> v * RandomUtils.randomExponential(rate));
     }
 }
